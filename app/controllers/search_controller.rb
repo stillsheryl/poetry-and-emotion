6f291f9862a5720c31eefb410bf53ff9227c794e
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-08 09:45:47 -0700

    Add controller action and update view to show poem data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
+    response = Faraday.get("https://poetrydb.org/author/#{params[:author]}")
 
+    poems = JSON.parse(response.body, symbolize_names: true)
+
+    @poems = poems.map do |poem|
+      Poem.new(poem)
+    end
   end
 end

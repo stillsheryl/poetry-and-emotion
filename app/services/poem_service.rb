097e4f9ef7203dd3b679a411a40b6173b6783f9b
Sheryl Stillman <sheryl.stillman@gmail.com>
2021-01-08 10:27:38 -0700

    Add poem service and test

---
+++ app/services/poem_service.rb
@@ -0,0 +1,7 @@
+class PoemService
+  def self.poems_by_author(author)
+    response = Faraday.get("https://poetrydb.org/author/#{author}")
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
---
+++ spec/services/poem_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "PoemService" do
+  it "retrieves tone from given text" do
+    poems = PoemService.poems_by_author("Emily")
+
+    expect(poems).to be_a(Array)
+
+    poem = poems.first
+    expect(poem).to be_a(Hash)
+    expect(poem).to have_key(:title)
+    expect(poem).to have_key(:author)
+    expect(poem).to have_key(:lines)
+  end
+end
